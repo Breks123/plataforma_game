808ea8c7c2b72d1335a24cd6cb6c179393a05491
Breks <157240460+Breks123@users.noreply.github.com>
2024-01-20 20:31:06 -0300

    versão 2

--- level_01/esmeralda.gd
+++
@@ -1,9 +0,0 @@
-extends Area2D
-
-
-func _on_items_body_entered(body: Node) -> void:
-	$anim.play("collected")
-
-func _on_animation_animation_finished(anim_name: String) -> void:
-	if anim_name == "collected": 
-		queue_free()
